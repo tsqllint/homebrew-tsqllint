baf425a497a5d336263c333f2ce73a43110fe853
Nathan Boyd <nboyd@salesforce.com>
2021-03-31 20:18:16 -0400

    pull latest stable artifacts

--- tsqllint.rb
+++ tsqllint.rb
@@ -5,13 +5,13 @@ class Tsqllint < Formula
   license "MIT"
 
   on_macos do
-      url "https://github.com/tsqllint/tsqllint/releases/download/v1.12.0-beta/osx-x64.tgz"
-      sha256 "38040253f89550b1f92b3260109cc38018cc8f843fbcbd4d50627ebd07c57115"
+      url "https://github.com/tsqllint/tsqllint/releases/download/v1.11.0/osx-x64.tgz"
+      sha256 "3dea0aae5941bcee514d8d09a92ff35c90efd6590f67ad1d3ae75ec5c1ba07c5"
   end
 
   on_linux do
-      url "https://github.com/tsqllint/tsqllint/releases/download/v1.12.0-beta/linux-x64.tgz"
-      sha256 "66874fe32ea4e721554340d709193bff4a9e747912bbfb1920da55329465c355"
+      url "https://github.com/tsqllint/tsqllint/releases/download/v1.11.0/linux-x64.tgz"
+      sha256 "3a76238d083af7e2153a9050ef82ba8f2c247e2b20484cd7a1a3c698c1bce834"
   end
 
   def install
